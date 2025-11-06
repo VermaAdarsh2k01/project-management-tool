db2debf2ef4534ebdc05373ded23092319ee9833
VermaAdarsh2k01 <verma.adarsh2510@gmail.com>
2025-11-06 19:00:59 +0530

    fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['@prisma/client']
+  serverExternalPackages: ['@prisma/client'],
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push({
+        '@prisma/client': 'commonjs @prisma/client'
+      })
+    }
+    return config
   }
 }
 
