bbd3fee88c19fcab41ea75cf0b5a88e20a960ce0
VermaAdarsh2k01 <verma.adarsh2510@gmail.com>
2025-11-06 18:55:57 +0530

    server issue fix

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ['@prisma/client', 'prisma'],
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals.push({
-        'utf-8-validate': 'commonjs utf-8-validate',
-        'bufferutil': 'commonjs bufferutil',
-      })
-    }
-    return config
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client']
   }
 }
 
